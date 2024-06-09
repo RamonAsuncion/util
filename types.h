243aa9c0cd6130efa304b2ac247d50336bf9ac45
Ramon Asuncion <ramon@b1t.dev>
2024-06-09 04:17:04 -0400

    Add macro and IWYU.

--- types.h
+++ types.h
@@ -1,14 +1,23 @@
 #ifndef _TYPES_H_
 #define _TYPES_H_
 
-#include <stdint.h>
-#include <stddef.h>
-#include <stdbool.h>
+#include <stdint.h>   /* IWYU pragma: export */
+#include <inttypes.h> /* IWYU pragma: export */
+#include <limits.h>   /* IWYU pragma: export */
+#include <string.h>   /* IWYU pragma: export */
 
-// Basic Types
+// get index of pointer within array by byte offset calculation.
+#define ARR_PTR_IDX(_p, _arr) (((char *)_p) - (_arr))
+
+// determine the length of a compile-time known array.
+#define ARR_LEN(_arr) (sizeof(_arr) / sizeof((_arr)[0]))
+
+// basic Types
 typedef float     f32;
+typedef double    f64;
+typedef unsigned int uint;
 
-// Fixed-width Types
+// fixed-width Types
 typedef int8_t    i8;
 typedef uint8_t   u8;
 typedef int16_t   i16;
@@ -18,7 +27,7 @@ typedef uint32_t  u32;
 typedef int64_t   i64;
 typedef uint64_t  u64;
 
-// Platform-specific Types
+// platform-specific Types
 typedef intptr_t  isize;
 typedef uintptr_t usize;
 
